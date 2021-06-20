56ee62875dc7fa876dc149ee0ea873c90fc5a412
Jack Desmond <techidesmond@gmail.com>
2021-06-20 20:28:16 +0100

    Adds Validation to Account file.
    Adds validation to Account class for when user Registers or Logins

---
+++ includes/classes/Account.php
@@ -0,0 +1,22 @@
+<?php
+    class Account
+    {
+        private $con;
+        //Sets empty array for error message.
+        private $errorArray = array();
+
+        public function __construct($con)
+        {
+            $this->con = $con;
+        }
+        
+        //Checks length of first name is more than 25 chars long.
+        public function validateFirstName($fn) {
+            if(strlen($fn) < 2 || strlen($fn) > 25) {
+                array_push($this->errorArray, "First name wrong lenth");
+            }
+
+        }
+    }
+    
+?>
